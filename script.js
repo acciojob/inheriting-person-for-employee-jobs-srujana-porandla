07eda59f7880c8e4d47e6cd7bc91e5379c9931e0
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-26 21:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Person{
 		this.age=age;
 	}
 	greet(){
-		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	}
 }
 
@@ -15,7 +15,7 @@ class Employee extends Person{
 		this.jobTitle=jobTitle;
 	}
 	jobGreet(){
-		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
 }
 const person = new Person('Alice', 25);
